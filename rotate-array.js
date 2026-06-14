4e27ecad47d6f9acf27d68fff4260fa9031b64f4
iryna novykova <irynan@wix.com>
2026-06-14 15:46:21 +0300

    rotate-array initial solution

---
+++ rotate-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    const tail = nums.splice(-k);
+    nums.unshift(...tail);
+console.log(nums); //
+};
+
+// Input: nums = [1,2,3,4,5,6,7], k = 3
+// Output: [5,6,7,1,2,3,4]
+
+// Input: nums = [-1,-100,3,99], k = 2
+// Output: [3,99,-1,-100]
+
+// input [1,2]
+// [2, 1]
+console.log(rotate([1,2], 7)); //
