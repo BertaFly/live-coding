fd45d57b217edcee25ca47a4bac34950091f3b13
ira.novykova <irrina.novikova@gmail.com>
2026-06-15 23:59:44 +0300

    chore(search-insert-position): pass all tests need refactor

--- search-insert-position.js
+++ search-insert-position.js
@@ -3,38 +3,31 @@
  * @param {number} target
  * @return {number}
  */
-const findIndexInNums = (nums, target, start, end) => {
-    let i = start;
-    while (i <= end) {
-        if (nums[i] >= target) return i;
-        i++;
-    }
-}
-
 var searchInsert = function(nums, target) {
     const length = nums.length;
+    if (length === 1) {
+        return target <= nums[0] ? 0 : 1;
+    }
     let start = 0;
     let middle = Math.floor(length / 2);
     let end = length - 1;
-    console.log('args', nums, target);
     while (target !== nums[middle]) {
-        console.log('start', start, 'middle', middle, 'end', end);
         if (target > nums[middle]) {
             start = middle;
         } else if (target < nums[middle]) {
             end = middle;
         }
         middle = Math.floor((start + end) / 2);
-        if (start === end) return middle;
-        if (end - start === 1) {
-            if (target > nums[start] && target < nums[end]) return end;
+        if (start === end || end - start === 1) {
+            if (target <= nums[start]) return start;
+            if (target === nums[end] || (target > nums[start] && target < nums[end])) return end;
             if (target > nums[end]) return end + 1;
         }
     }
     return middle;
 };
 
-console.log(searchInsert([1,3,5,6], 5));
+console.log(searchInsert([-1,3,5,6], 0));
 
 // Example 1:
 // Input: nums = [1,3,5,6], target = 5
@@ -47,3 +40,18 @@ console.log(searchInsert([1,3,5,6], 5));
 // Example 3:
 // Input: nums = [1,3,5,6], target = 7
 // Output: 4
+
+// [1], target = 2
+// Output: 1
+
+// [1001], target = 5;
+// Output: 0
+
+// [1,3,5], target = 5
+// Output: 2
+
+// [2,5], target = 1
+// Output: 0
+
+// [-1,3,5,6], target = 0;
+// Output: 1
